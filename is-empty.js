9cca458a06c1504e20320c7db251dc395dda729c
Rob Last <robert.last@gmail.com>
2018-01-03 06:12:22 +0000

    Added Docs6

--- is-empty.js
+++ is-empty.js
@@ -1,9 +1,9 @@
 "use strict";
 /**
  * Is the given value an empty string or undefined?
- * @function isEmpty
  * @param {string} val - The value to check.
  * @returns {boolean}
+ * @function isEmpty
  */
 module.exports = function isEmpty(val) {
 	return (val === '' || val === undefined);
